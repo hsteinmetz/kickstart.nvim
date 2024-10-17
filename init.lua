b3145d86b8b6e96666d91d398d3f6340fc8fb5c2
Hendrik Steinmetz <hendrik_steinmetz@live.com>
2024-10-17 19:22:44 +0200

    Fixed keymap module import.

--- init.lua
+++ init.lua
@@ -1054,4 +1054,4 @@ vim.api.nvim_create_autocmd('FileType', {
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
-require 'lua/keymaps'
+require 'keymaps'
