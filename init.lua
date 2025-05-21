346e4dfb257ba5df2a9967b3cc22165aad09642a
Hendrik Steinmetz <hendrik_steinmetz@live.com>
2025-05-21 10:26:50 +0200

    updated plugins and fixed vue ls mason problem

--- init.lua
+++ init.lua
@@ -498,7 +498,7 @@ require('lazy').setup({
       }
 
       local mason_registry = require 'mason-registry'
-      local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
+      local vue_language_server_path = vim.fn.expand '$MASON/packages/vue-language-server' .. '/node_modules/@vue/language-server'
       require('lspconfig').ts_ls.setup {
         init_options = {
           plugins = {
@@ -833,6 +833,12 @@ require('lazy').setup({
         },
       },
     },
+  },
+  {
+    'm4xshen/hardtime.nvim',
+    lazy = false,
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    opts = {},
   },
 
   -- require 'kickstart.plugins.debug',
