0db74ef8724fced595613b7d35aba1845bd28c14
Hendrik Steinmetz <hendrik_steinmetz@live.com>
2025-09-04 14:36:19 +0200

    keymap cleanup

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,59 +1,64 @@
+local map = vim.keymap.set
+local mapn = function(lhs, rhs, opts)
+  map('n', lhs, rhs, opts)
+end
+
 -- Tabs
-vim.keymap.set('n', '<leader><tab>l', '<cmd>tablast<cr>', { desc = 'Last tab' })
-vim.keymap.set('n', '<leader><tab>f', '<cmd>tabfirst<cr>', { desc = 'First tab' })
-vim.keymap.set('n', '<leader><tab>d', '<cmd>tabclose<cr>', { desc = 'Close tab' })
-vim.keymap.set('n', '<leader><tab>[', '<cmd>tabprevious<cr>', { desc = 'Previous tab' })
-vim.keymap.set('n', '<leader><tab>]', '<cmd>tabnext<cr>', { desc = 'Next tab' })
-vim.keymap.set('n', '<leader><tab><tab>', '<cmd>tabnew<cr>', { desc = 'Create new tab' })
+mapn('<leader><tab>l', '<cmd>tablast<cr>', { desc = 'Last tab' })
+mapn('<leader><tab>f', '<cmd>tabfirst<cr>', { desc = 'First tab' })
+mapn('<leader><tab>d', '<cmd>tabclose<cr>', { desc = 'Close tab' })
+mapn('<leader><tab>[', '<cmd>tabprevious<cr>', { desc = 'Previous tab' })
+mapn('<leader><tab>]', '<cmd>tabnext<cr>', { desc = 'Next tab' })
+mapn('<leader><tab><tab>', '<cmd>tabnew<cr>', { desc = 'Create new tab' })
 
 -- Text editing
-vim.keymap.set('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move line up' })
-vim.keymap.set('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move line down' })
+mapn('<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move line up' })
+mapn('<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move line down' })
 vim.keymap.set('i', '<A-j>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move line down' })
 vim.keymap.set('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move line down' })
 
 -- General
-vim.keymap.set('n', '<leader>fn', '<cmd>enew<cr>')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', { desc = 'Save file' })
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>', { desc = 'Reload config' })
+mapn('<leader>fn', '<cmd>enew<cr>')
+mapn('<C-d>', '<C-d>zz')
+mapn('<C-u>', '<C-u>zz')
+mapn('<leader>w', '<cmd>w<cr>', { desc = 'Save file' })
+mapn('<leader>o', ':update<CR> :source<CR>', { desc = 'Reload config' })
 
 -- Windows
-vim.keymap.set('n', '<leader>-', '<C-W>s', { desc = 'Split horizontally' })
-vim.keymap.set('n', '<leader>|', '<C-W>v', { desc = 'Split vertically' })
-vim.keymap.set('n', '<leader>wd', '<C-W>c', { desc = 'Delete window' })
-vim.keymap.set('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
-vim.keymap.set('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
-vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
-vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
-vim.keymap.set('n', '<leader>wl', '<cmd>wincmd l<cr>', { desc = 'Got to right window', remap = true })
-vim.keymap.set('n', '<leader>wh', '<cmd>wincmd h<cr>', { desc = 'Got to left window' })
-vim.keymap.set('n', '<leader>wk', '<cmd>wincmd k<cr>', { desc = 'Got to upper window' })
-vim.keymap.set('n', '<leader>wj', '<cmd>wincmd j<cr>', { desc = 'Got to lower window' })
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+mapn('<leader>-', '<C-W>s', { desc = 'Split horizontally' })
+mapn('<leader>|', '<C-W>v', { desc = 'Split vertically' })
+mapn('<leader>wd', '<C-W>c', { desc = 'Delete window' })
+mapn('<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
+mapn('<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
+mapn('<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
+mapn('<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
+mapn('<leader>wl', '<cmd>wincmd l<cr>', { desc = 'Got to right window', remap = true })
+mapn('<leader>wh', '<cmd>wincmd h<cr>', { desc = 'Got to left window' })
+mapn('<leader>wk', '<cmd>wincmd k<cr>', { desc = 'Got to upper window' })
+mapn('<leader>wj', '<cmd>wincmd j<cr>', { desc = 'Got to lower window' })
+mapn('<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+mapn('<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+mapn('<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+mapn('<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- telescope
 local builtin = require 'telescope.builtin'
-vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>sy', builtin.lsp_document_symbols, { desc = '[S]earch document S[y]mbols' })
-vim.keymap.set('n', '<leader>sY', builtin.lsp_workspace_symbols, { desc = '[S]earch workspace S[Y]mbols' })
-vim.keymap.set('n', '<leader>sm', builtin.marks, { desc = '[S]earch [M]arks' })
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, { desc = '[L]SP Format' })
+mapn('<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+mapn('<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+mapn('<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+mapn('<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+mapn('<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+mapn('<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+mapn('<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+mapn('<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+mapn('<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+mapn('<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+mapn('<leader>sy', builtin.lsp_document_symbols, { desc = '[S]earch document S[y]mbols' })
+mapn('<leader>sY', builtin.lsp_workspace_symbols, { desc = '[S]earch workspace S[Y]mbols' })
+mapn('<leader>sm', builtin.marks, { desc = '[S]earch [M]arks' })
+mapn('<leader>lf', vim.lsp.buf.format, { desc = '[L]SP Format' })
 
-vim.keymap.set('n', '<leader>/', function()
+mapn('<leader>/', function()
   -- You can pass additional configuration to Telescope to change the theme, layout, etc.
   builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
@@ -63,7 +68,7 @@ end, { desc = '[/] Fuzzily search in current buffer' })
 
 -- It's also possible to pass additional configuration options.
 --  See `:help telescope.builtin.live_grep()` for information about particular keys
-vim.keymap.set('n', '<leader>s/', function()
+mapn('<leader>s/', function()
   builtin.live_grep {
     grep_open_files = true,
     prompt_title = 'Live Grep in Open Files',
@@ -71,11 +76,11 @@ vim.keymap.set('n', '<leader>s/', function()
 end, { desc = '[S]earch [/] in Open Files' })
 
 -- Shortcut for searching your Neovim configuration files
-vim.keymap.set('n', '<leader>sn', function()
+mapn('<leader>sn', function()
   builtin.find_files { cwd = vim.fn.stdpath 'config' }
 end, { desc = '[S]earch [N]eovim files' })
 
 vim.diagnostic.config {
   virtual_text = true,
 }
-vim.keymap.set('n', '<leader>lq', ':lua vim.diagnostic.open_float()<cr>', { desc = '[L]sp [Q]uickfix' })
+mapn('<leader>lq', ':lua vim.diagnostic.open_float()<cr>', { desc = '[L]sp [Q]uickfix' })
