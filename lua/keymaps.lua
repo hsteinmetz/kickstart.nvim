d34faa0180664cc423fe64ae6c15e98d16d447e6
Hendrik Steinmetz <hendrik_steinmetz@live.com>
2025-09-04 19:55:20 +0200

    config split

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -24,6 +24,15 @@ mapn('<C-u>', '<C-u>zz')
 mapn('<leader>w', '<cmd>w<cr>', { desc = 'Save file' })
 mapn('<leader>o', ':update<CR> :source<CR>', { desc = 'Reload config' })
 mapn('<leader>e', ':Oil<CR>', { desc = 'Open file explorer' })
+mapn('<Esc>', '<cmd>nohlsearch<CR>')
+mapn('<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- Windows
 mapn('<leader>-', '<C-W>s', { desc = 'Split horizontally' })
